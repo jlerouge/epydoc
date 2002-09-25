51e54581b2f77c60434617852a4a35a0d5d8898c
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2002-09-25 20:25:44 +0000

    scripts for epydoc's graphical and command line interfaces

---
+++ src/scripts/epydoc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# Call the command line interface for Epydoc.
+#
+
+# We have to do some path magic to prevent Python from getting
+# confused about the difference between this epydoc module, and the
+# real epydoc package.
+import sys, os, os.path
+cwd = os.path.abspath(os.curdir)
+sys.path = [d for d in sys.path if os.path.abspath(d) != cwd]
+
+from epydoc.cli import cli
+cli()
+
---
+++ src/scripts/epydoc.pyw
@@ -0,0 +1,13 @@
+# Call the graphical interface for Epydoc.
+#
+
+# We have to do some path magic to prevent Python from getting
+# confused about the difference between this epydoc module, and the
+# real epydoc package.
+import sys, os, os.path
+cwd = os.path.abspath(os.curdir)
+sys.path = [d for d in sys.path if os.path.abspath(d) != cwd]
+
+from epydoc.gui import gui
+gui()
+
