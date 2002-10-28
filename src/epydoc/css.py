23cc93b61423762a4e42a72957bfda608198d478
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2002-10-28 18:03:09 +0000

    - Changed .summary-sig-name

--- src/epydoc/css.py
+++ src/epydoc/css.py
@@ -79,7 +79,7 @@ h3.var-details     { background: transparent; color: #000000;
                    { background: transparent; color: #008060; }  
 .sig-default       { background: transparent; color: #602000; }  
 .summary-sig       { background: transparent; color: #000000; }  
-.summary-sig-name  { background: transparent; font-weight: bold; }  
+.summary-sig-name  { background: transparent; color: #0000ff; }
 .summary-sig-arg, .summary-sig-kwarg, .summary-sig-vararg
                    { background: transparent; color: #008060; }  
 
@@ -177,7 +177,7 @@ h3.var-details     { background: transparent; color: #000000;
                    { background: transparent; color: #008060; }  
 .sig-default       { background: transparent; color: #602000; }  
 .summary-sig       { background: transparent; color: #000000; }  
-.summary-sig-name  { background: transparent; font-weight: bold; }  
+.summary-sig-name  { background: transparent; color: #104060; }
 .summary-sig-arg, .summary-sig-kwarg, .summary-sig-vararg
                    { background: transparent; color: #008060; }  
 
