03ec188ce7dc4a00825fc46689f94a8ff4114851
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2002-11-05 09:29:39 +0000

    - Added --dvi, --ps, --pdf - Consolidated progress bar code into a single class (_Progress).

--- src/epydoc/cli.py
+++ src/epydoc/cli.py
@@ -48,13 +48,26 @@ true, then L{cli} will issue a warning once it completes running.
 """
 __docformat__ = 'epytext en'
 
-import sys, os.path, re, getopt
 
+##################################################
+## Constants
+##################################################
+
+# Use "%(tex)s" to include the latex filename, "%(ps)s" for the
+# postscript filename, etc.  (You must include the "s" after the close
+# parenthasis).
+LATEX_COMMAND = 'latex %(tex)s'
+MAKEINDEX_COMMAND = 'makeindex -q %(idx)s'
+DVIPS_COMMAND = 'dvips -q %(dvi)s -o %(ps)s -G0 -Ppdf'
+PS2PDF_COMMAND = ('ps2pdf -sPAPERSIZE=letter -dMaxSubsetPct=100 '+
+                  '-dSubsetFonts=true -dCompatibilityLevel=1.2 '+
+                  '-dEmbedAllFonts=true %(ps)s %(pdf)s')
 PROFILE=0
 
 ##################################################
 ## Command-Line Interface
 ##################################################
+import sys, os.path, re, getopt
 
 def cli():
     """
@@ -72,9 +85,13 @@ def cli():
     docmap = _make_docmap(modules, options)
 
     # Perform the requested action.
-    if options['check']: _check(docmap, options)
-    elif options['latex']: _latex(docmap, options)
-    else: _html(docmap, options)
+    if options['action'] == 'html': _html(docmap, options)
+    elif options['action'] == 'check': _check(docmap, options)
+    elif options['action'] == 'latex': _latex(docmap, options, 'latex')
+    elif options['action'] == 'dvi': _latex(docmap, options, 'dvi')
+    elif options['action'] == 'ps': _latex(docmap, options, 'ps')
+    elif options['action'] == 'pdf': _latex(docmap, options, 'pdf')
+    else: raise ValueError('Unknown action %r' % options['action'])
 
     # Report any internal errors.
     if _encountered_internal_error:
@@ -169,18 +186,16 @@ def _parse_args():
         values.  If a parameter is specified on the command line, then
         that value is used; otherwise, a default value is used.
         Currently, the following configuration parameters are set:
-        C{target}, C{modules}, C{verbosity}, C{prj_name}, C{check},
+        C{target}, C{modules}, C{verbosity}, C{prj_name}, C{output},
         C{check_private}, C{show_imports}, C{frames}, C{private},
         C{debug}, C{top}, and C{docformat}.
     @rtype: C{None}
     """
     # Default values.
     options = {'target':'html', 'modules':[], 'verbosity':1,
-               'prj_name':'', 'check':0, 'check_private':0,
+               'prj_name':'', 'action':'html', 'check_private':0,
                'show_imports':0, 'frames':1, 'private':1,
-               'debug':0, 'docformat':None, 'top':None,
-               'latex':0}
-               
+               'debug':0, 'docformat':None, 'top':None}
 
     # Get the command-line arguments, using getopts.
     shortopts = 'c:fh:n:o:t:u:Vvpq?:'
@@ -190,7 +205,7 @@ def _parse_args():
                 'show_imports css= no_private no-private name= '+
                 'builtins no-frames no_frames noframes debug '+
                 'docformat= doc-format= doc_format= top=  navlink= '+
-                'nav_link= nav-link= latex').split()
+                'nav_link= nav-link= latex html dvi ps pdf').split()
     try:
         (opts, modules) = getopt.getopt(sys.argv[1:], shortopts, longopts)
     except getopt.GetoptError, e:
@@ -201,9 +216,10 @@ def _parse_args():
 
     # Parse the arguments.
     for (opt, val) in opts:
-        if opt in ('--check',): options['check'] = 1
+        if opt in ('--check',): options['action'] = 'check'
         elif opt in ('--css', '-c'): options['css'] = val
         elif opt in ('--debug',): options['debug']=1
+        elif opt in ('--dvi',): options['action'] = 'dvi'
         elif opt in ('--docformat', '--doc-format', '--doc_format'):
             from epydoc.objdoc import set_default_docformat
             set_default_docformat(val)
@@ -215,15 +231,18 @@ def _parse_args():
         elif opt in ('--help', '-?', '--usage', '-h'): _help(val)
         elif opt in ('--helpfile', '--help-file', '--help_file'):
             options['help'] = val
-        elif opt in ('--latex',): options['latex']=1
+        elif opt in ('--html',): options['action'] = 'html'
+        elif opt in ('--latex',): options['action']='latex'
         elif opt in ('--name', '-n'): options['prj_name']=val
         elif opt in ('--navlink', '--nav-link', '--nav_link'):
             options['prj_link'] = val
         elif opt in ('--no-private', '--no_private'): options['private']=0
         elif opt in ('--output', '--target', '-o'): options['target']=val
         elif opt in ('-p',): options['check_private'] = 1
+        elif opt in ('--pdf',): options['action'] = 'pdf'
         elif opt in ('--private-css', '--private_css'):
             options['private_css'] = val
+        elif opt in ('--ps',): options['action'] = 'ps'
         elif opt in ('--quiet', '-q'): options['verbosity'] -= 1
         elif opt in ('--show-imports', '--show_imports'):
             options['show_imports'] = 1
@@ -271,25 +290,10 @@ def _find_modules(module_names, verbosity):
     if verbosity > 0:
         print >>sys.stderr, 'Importing %s modules.' % len(module_names)
     modules = []
-    module_num = 1
-    num_modules = len(module_names)
-    TRACE_FORMAT = ("  [%%%dd/%d] Importing %%s" %
-                    (len(`num_modules`), num_modules))
+    progress = _Progress('Importing', verbosity, len(module_names))
+    
     for name in module_names:
-        if verbosity == 1:
-            if module_num == 1 and num_modules <= 70: sys.stderr.write('  [')
-            if (module_num % 60) == 1 and num_modules > 70:
-                sys.stderr.write('  [%3d%%] ' % (100.0*module_num/num_modules))
-            sys.stderr.write('.')
-            sys.stderr.softspace = 1
-            if (module_num % 60) == 0 and num_modules > 70: print >>sys.stderr
-            if module_num == num_modules:
-                if num_modules <= 70: sys.stderr.write(']')
-                print >>sys.stderr
-        elif verbosity > 1:
-            print  >>sys.stderr, TRACE_FORMAT % (module_num, name)
-        module_num += 1
-
+        progress.report(name)
         # Import the module, and add it to the list.
         try:
             module = import_module(name)
@@ -301,6 +305,9 @@ def _find_modules(module_names, verbosity):
             if verbosity >= 0: 
                 print  >>sys.stderr, '\n  Warning: %s' % e
 
+    if len(modules) == 0:
+        print >>sys.stderr, 'Error: no modules successfully loaded!'
+        sys.exit(1)
     return modules
 
 def _make_docmap(modules, options):
@@ -314,34 +321,16 @@ def _make_docmap(modules, options):
     """
     from epydoc.objdoc import DocMap
 
-    # Don't bother documenting base classes if we're just running
-    # checks.
-    quiet = -1 * (options['verbosity'] < 0)
-    document_bases = not options['check']
-    d = DocMap(quiet, document_bases)
-    
-    num_modules = len(modules)
+    verbosity = options['verbosity']
+    document_bases = (options['action'] != 'check')
+    d = DocMap(verbosity, document_bases)
     if options['verbosity'] > 0:
         print  >>sys.stderr, ('Building API documentation for %d modules.'
-                              % num_modules)
-    TRACE_FORMAT = ("  [%%%dd/%d] Building docs for %%s" %
-                    (len(`num_modules`), num_modules))
-    module_num = 1
+                              % len(modules))
+    progress = _Progress('Building docs for', verbosity, len(modules))
+    
     for module in modules:
-        if options['verbosity'] == 1:
-            if module_num == 1 and num_modules <= 70: sys.stderr.write('  [')
-            if (module_num % 60) == 1 and num_modules > 70:
-                sys.stderr.write('  [%3d%%] ' % (100.0*module_num/num_modules))
-            sys.stderr.write('.')
-            sys.stderr.softspace = 1
-            if (module_num % 60) == 0 and num_modules > 70: print >>sys.stderr
-            if module_num == num_modules:
-                if num_modules <= 70: sys.stderr.write(']')
-                print >>sys.stderr
-        elif options['verbosity'] > 1:
-            print >>sys.stderr, TRACE_FORMAT % (module_num, module.__name__)
-        module_num += 1
-
+        progress.report(module.__name__)
         # Add the module.  Catch any exceptions that get generated.
         try: d.add(module)
         except Exception, e:
@@ -353,7 +342,22 @@ def _make_docmap(modules, options):
                 
     return d
 
-def _latex(docmap, options):
+def _run(cmd, options):
+    from epydoc.epytext import wordwrap
+    name = cmd.split(' ', 1)[0]
+    if options['verbosity'] == 1:
+        print >>sys.stderr, 'Running %s...' % name
+    elif options['verbosity'] > 1:
+        cmd_str = wordwrap(`cmd`, 10+len(name)).lstrip()
+        print >>sys.stderr, 'Running %s' % cmd_str.rstrip()
+    pipe = os.popen(cmd)
+    output = pipe.read()
+    exitcode = pipe.close()
+    if exitcode is not None:
+        print output
+        raise OSError('%s failed: %s' % name, exitcode)
+
+def _latex(docmap, options, format):
     """
     Create the LaTeX documentation for the objects in the given
     documentation map.  
@@ -362,19 +366,52 @@ def _latex(docmap, options):
         for the objects whose API documentation should be created.
     @param options: Options from the command-line arguments.
     @type options: C{dict}
+    @param format: One of C{'latex'}, C{'dvi'}, C{'ps'}, or C{'pdf'}.
     """
     from epydoc.latex import LatexFormatter
 
     # Create the documenter, and figure out how many files it will
     # generate.
-    htmldoc = LatexFormatter(docmap, **options)
-    num_files = 10
+    latex_doc = LatexFormatter(docmap, **options)
+    num_files = latex_doc.num_files()
         
     # Write documentation.
     if options['verbosity'] > 0:
-        print  >>sys.stderr, ('Writing API documentation (%d files) to %s.' %
+        print  >>sys.stderr, ('Writing LaTeX docs (%d files) to %r.' %
                               (num_files, options['target']))
-    htmldoc.dvi(options['target'])
+    progress = _Progress('Writing', options['verbosity'], num_files)
+    latex_doc.write(options['target'], progress.report)
+
+    # Run latex, makeindex, dvi, ps, and pdf, as appropriate.
+    oldpath = os.path.abspath(os.curdir)
+    try:
+        try:
+            # Filenames (used by the external commands)
+            filenames = {'tex': 'api.tex', 'idx': 'api.idx',
+                         'dvi': 'api.dvi', 'ps': 'api.ps',
+                         'pdf': 'api.pdf'}
+        
+            # latex -> dvi
+            if format in ('dvi', 'ps', 'pdf'):
+                # Go into the output directory.
+                os.chdir(options['target'])
+                
+                _run(LATEX_COMMAND % filenames, options)
+                _run(MAKEINDEX_COMMAND % filenames, options)
+                _run(LATEX_COMMAND % filenames, options)
+                
+            # dvi -> postscript
+            if format in ('ps', 'pdf'):
+                _run(DVIPS_COMMAND % filenames, options)
+
+            # postscript -> pdf
+            if format in ('pdf',): 
+                _run(PS2PDF_COMMAND % filenames, options)
+
+        except OSError, e:
+            print  >>sys.stderr, 'Error: %s' % e
+    finally:
+        os.chdir(oldpath)
 
 def _html(docmap, options):
     """
@@ -390,37 +427,15 @@ def _html(docmap, options):
 
     # Create the documenter, and figure out how many files it will
     # generate.
-    htmldoc = HTMLFormatter(docmap, **options)
-    num_files = htmldoc.num_files()
-        
-    # Produce pretty trace output.
-    def progress_callback(path, doc, verbosity=options['verbosity'],
-                          num_files=num_files, file_num=[1]):
-        if verbosity==1:
-            if file_num[0] == 1 and num_files <= 70: sys.stderr.write('  [')
-            if (file_num[0] % 60) == 1 and num_files > 70:
-                sys.stderr.write('  [%3d%%] ' % (100.0*file_num[0]/num_files))
-            sys.stderr.write('.')
-            sys.stderr.softspace = 1
-            if (file_num[0] % 60) == 0 and num_files > 70: print >>sys.stderr
-            if file_num[0] == num_files:
-                if num_files <= 70: sys.stderr.write(']')
-                print >>sys.stderr
-        elif verbosity>1:
-            TRACE_FORMAT = (('  [%%%dd/%d]' % (len(`num_files`), num_files)) +
-                            ' Writing %s')
-            (dir, file) = os.path.split(path)
-            (root, d) = os.path.split(dir)
-            if d in ('public', 'private'): fname = os.path.join(d, file)
-            else: fname = file
-            print >>sys.stderr, TRACE_FORMAT % (file_num[0], fname)
-        file_num[0] += 1
-        
+    html_doc = HTMLFormatter(docmap, **options)
+    num_files = html_doc.num_files()
+
     # Write documentation.
     if options['verbosity'] > 0:
-        print  >>sys.stderr, ('Writing API documentation (%d files) to %s.' %
+        print  >>sys.stderr, ('Writing HTML docs (%d files) to %r.' %
                               (num_files, options['target']))
-    htmldoc.write(options['target'], progress_callback)
+    progress = _Progress('Writing', options['verbosity'], num_files, 1)
+    html_doc.write(options['target'], progress.report)
 
 def _check(docmap, options):
     """
@@ -446,6 +461,73 @@ def _check(docmap, options):
         checker.check(DocChecker.ALL_T | DocChecker.PUBLIC | 
                       DocChecker.DESCR_LAZY | DocChecker.TYPE)
            
+class _Progress:
+    """
+
+    The progress meter that is used by C{cli} to report its progress.
+    It prints the status to C{stderrr}.  Depending on the verbosity,
+    setting it will produce different outputs.
+
+    To update the progress meter, call C{report} with the name of the
+    object that is about to be processed.
+    """
+    def __init__(self, action, verbosity, total_items, html_file=0):
+        """
+        Create a new progress meter.
+
+        @param action: A string indicating what action is performed on
+            each objcet.  Examples are C{"writing"} and C{"building
+            docs for"}.
+        @param verbosity: The verbosity level.  This controls what the
+            progress meter output looks like.
+        @param total_items: The total number of items that will be
+            processed with this progress meter.  This is used to let
+            the user know how much progress epydoc has made.
+        @param html_file: Whether to assume that arguments are html
+            file names, and munge them appropriately.
+        """
+        self._action = action
+        self._verbosity = verbosity
+        self._total_items = total_items
+        self._item_num = 1
+        self._html_file = 0
+
+    def report(self, argument):
+        """
+        Update the progress meter.
+        @param argument: The object that is about to be processed.
+        """
+        if self._verbosity <= 0: return
+        
+        if self._verbosity==1:
+            if self._item_num == 1 and self._total_items <= 70:
+                sys.stderr.write('  [')
+            if (self._item_num % 60) == 1 and self._total_items > 70:
+                sys.stderr.write('  [%3d%%] ' %
+                                 (100.0*self._item_num/self._total_items))
+            sys.stderr.write('.')
+            sys.stderr.softspace = 1
+            if (self._item_num % 60) == 0 and self._total_items > 70:
+                print >>sys.stderr
+            if self._item_num == self._total_items:
+                if self._total_items <= 70: sys.stderr.write(']')
+                print >>sys.stderr
+        elif self._verbosity>1:
+            TRACE_FORMAT = (('  [%%%dd/%d]' % (len(`self._total_items`),
+                                               self._total_items))+
+                            ' %s %%s' % self._action)
+
+            if self._html_file:
+                (dir, file) = os.path.split(argument)
+                (root, d) = os.path.split(dir)
+                if d in ('public', 'private'):
+                    argument = os.path.join(d, file)
+                else:
+                    fname = argument
+            
+            print >>sys.stderr, TRACE_FORMAT % (self._item_num, argument)
+        self._item_num += 1
+        
 if __name__ == '__main__':
     if PROFILE:
         from profile import Profile
