a6f99a07d705d67c0fdda6c287576eff7f073068
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2002-11-07 08:10:33 +0000

    - import_module no longer accepts directories. - Added find_modules, which will search a directory for the package   file, and all of its descendant packages & modules.

--- src/epydoc/imports.py
+++ src/epydoc/imports.py
@@ -8,13 +8,168 @@
 #
 
 """
-Module import support for epydoc.  C{imports} defines a single public
-function, C{import_module}, which finds and imports a module, given
-its module name or its file name.
+Module import support for epydoc.
 """
 __docformat__ = 'epytext en'
 
 import sys, re, types, os.path
+
+_VALID_MODULE_NAME = re.compile(r'^[a-zA-Z_]\w*(\.[a-zA-Z_]\w*)*$')
+
+def find_modules(dirname):
+    """
+    If C{dirname} contains a package, then return the filenames for
+    the package, its modules, and all of its descendant packages and
+    modules.  If C{dirname} does not contain a package, then return an
+    empty list.
+
+    @rtype: C{list} of C{string}
+    @return: The filenames for the package in C{dirname}, its modules,
+        and all of its descendant packages and modules.
+    @type C{dirname}: C{string}
+    @param C{dirname}: The directory name to search.
+    """
+    if not os.path.isdir(dirname): return []
+    found_init = 0
+    modules = {}
+    dirs = []
+
+    # Search for directories & modules, and check for __init__.py.
+    # Don't include duplicates (like foo.py and foo.pyc), and give
+    # precedance to the .py files.
+    for file in os.listdir(dirname):
+        filepath = os.path.join(dirname, file)
+        if os.path.isdir(filepath): dirs.append(filepath)
+        elif file[-3:] == '.py':
+            modules[file] = os.path.join(dirname, file)
+            if file == '__init__.py': found_init = 1
+        elif file[-4:-1] == '.py':
+            modules.setdefault(file[:-1], file)
+            if file[:-1] == '__init__.py': found_init = 1
+    modules = modules.values()
+
+    # If there was no __init__.py, then this isn't a package
+    # directory; return nothing.
+    if not found_init: return []
+
+    # Recurse to the child directories.
+    for d in dirs: modules += find_modules(d)
+    return modules
+
+def import_module(name_or_filename):
+    """
+    @return: The module with the given module name or filename.
+    
+        - If C{name_or_filename} is the name of a module (such as
+          C{os.path}), then return that module.
+        - If C{name_or_filename} is the name of a file (such as
+          C{epytext.py} or C{multiarray.so}), then return the module
+          defined by that file.
+
+    If C{name_or_filename} names both a file and a module, then
+    C{import_module} will treat it as a file name.
+    
+    @rtype: C{list} of C{module}
+    @type name_or_filename: C{string}
+    @param name_or_filename: The module name or filename specifying
+        which module to import.
+    @raise ImportError: If there was any problem importing the given
+        object.  In particular, an C{ImportError} is raised if the
+        given file does not exist; if the given file name does not
+        name a valid module; or if importing the module causes any
+        exception to be raised.
+    """
+    # If we've already imported it, then just return it.
+    if sys.modules.has_key(name_or_filename):
+        return sys.modules[name_or_filename]
+    
+    # Try importing it as a file name.
+    if os.path.exists(name_or_filename):
+        old_sys_path = sys.path[:]
+        (basedir, name) = _find_module_from_filename(name_or_filename)
+        sys.path.insert(0, basedir)
+        try: return _import_module(name)
+        finally: sys.path = old_sys_path
+
+    # Try importing it as a module name.
+    if _VALID_MODULE_NAME.match(name_or_filename):
+        return _import_module(name_or_filename)
+
+    # Report an error.
+    raise ImportError('Error importing %r: ' % name_or_filename +
+                      'bad module name or file not found.')
+
+def _import_module(name):
+    """
+    @return: the module with the given name.  C{import_module} makes
+        some attempts to prevent the imported module from modifying
+        C{sys} and C{__builtins__}.  In the future, more sandboxing
+        might be added (e.g., using the C{rexec} module).
+    @rtype: C{module}
+    @param name: The name of the module to import.  C{name} is a
+        fully qualified module name, such as C{os.path}.
+    @type name: C{string}
+    @raise ImportError: If there was any problem importing the given
+        object.  In particular, if importing the module causes any
+        exception to be raised.
+    """
+    # If we've already imported it, then just return it.
+    if sys.modules.has_key(name): return sys.modules[name]
+
+    # Save some important values.  This helps prevent the module that
+    # we import from breaking things *too* badly.  Note that we do
+    # *not* save sys.modules.
+    old_sys = sys.__dict__.copy()
+    old_sys_path = sys.path[:]
+    if type(__builtins__) == types.DictionaryType:
+        old_builtins = __builtins__.copy()
+    else:
+        old_builtins = __builtins__.__dict__.copy()
+
+    # Supress input and output.  (These get restored when we restore
+    # sys to old_sys).  
+    sys.stdin = sys.stdout = sys.stderr = _dev_null
+    sys.__stdin__ = sys.__stdout__ = sys.__stderr__ = _dev_null
+
+    # Remove any command-line arguments
+    sys.argv = ['(imported)']
+
+    try:
+        # Make sure that we have a valid name
+        if not _VALID_MODULE_NAME.match(name):
+            raise ImportError('Error importing %r: bad module name' % name)
+            
+        # Import the module.  Note that if "name" has a package
+        # component, then this just gives us the top-level object.
+        try:
+            topLevel = __import__(name)
+        except KeyboardInterrupt:
+            raise # don't capture keyboard interrupts!
+        except Exception, e:
+            raise ImportError('Error importing %r: %s' % (name, e))
+        except SystemExit, e:
+            raise ImportError('Error importing %r: %s' % (name, e))
+        except:
+            raise ImportError('Error importing %r')
+        
+        # If "name" has a package component, then we have to manually
+        # go down the package tree.
+        pieces = name.split(".")[1:]
+        m = topLevel
+        for p in pieces:
+            try: m = getattr(m, p)
+            except AttributeError:
+                estr = 'Error importing %r: getattr failed' % name
+                raise ImportError(estr)
+        return m
+    finally:
+        # Restore the important values that we saved.
+        if type(__builtins__) == types.DictionaryType:
+            __builtins__.update(old_builtins)
+        else:
+            __builtins__.__dict__.update(old_builtins)
+        sys.__dict__.update(old_sys)
+        sys.path = old_sys_path
 
 class _DevNull:
     """
@@ -65,16 +220,22 @@ def _find_module_from_filename(filename):
     # Normalize the filename
     filename = os.path.normpath(os.path.abspath(filename))
 
-    if os.path.isdir(filename):
-        (basedir, module) = os.path.split(filename)
-    else:
-        (basedir, file) = os.path.split(filename)
-        (module, ext) = os.path.splitext(file)
-        if module == '__init__':
-            (basedir, module) = os.path.split(basedir)
+    # Split the file into (basedir, module, ext), and check the extension.
+    (basedir, file) = os.path.split(filename)
+    (module, ext) = os.path.splitext(file)
+    if not (ext[-3:] == '.py' or ext[-4:-1] == '.py' or
+            ext[-3:] == '.so'):
+        raise ImportError('Error importing %r: ' % filename +
+                          'not a Python module')
+
+    # Is it a package?
+    if module == '__init__':
+        (basedir, module) = os.path.split(basedir)
     
     # If there's a package, then find its base directory.
-    if os.path.exists(os.path.join(basedir, '__init__.py')):
+    if (os.path.exists(os.path.join(basedir, '__init__.py')) or
+        os.path.exists(os.path.join(basedir, '__init__.pyc')) or
+        os.path.exists(os.path.join(basedir, '__init__.pyw'))):
         package = []
         while os.path.exists(os.path.join(basedir, '__init__.py')):
             (basedir,dir) = os.path.split(basedir)
@@ -85,104 +246,15 @@ def _find_module_from_filename(filename):
 
     return (basedir, module)
 
-def import_module(name):
-    """    
-    Return the module identified by the given name.  C{import_module}
-    makes some attempts to prevent the imported module from modifying
-    C{sys} and C{__builtins__}.  In the future, more sandboxing might
-    be added (e.g., using the C{rexec} module).
-
-    @return: The module with the given name.
-    @rtype: C{module}
-    @param name: The name of the module to import.  C{name} can
-        either be a module name (such as C{os.path}); a filename (such
-        as C{epytext.py} or C{multiarray.so}); or a directory name
-        (such as C{site-packages/distutils}).
-    @type name: C{string}
-    @raise ImportError: If there was any problem importing the given
-        object.  In particular, an C{ImportError} is raised if the
-        given file does not exist; if the given file name does not
-        name a valid module; or if importing the module causes an
-        exception to be raised.
-    """
-    # If we've already imported it, then just return it.
-    if sys.modules.has_key(name):
-        return sys.modules[name]
-
-    # Save some important values.  This helps prevent the module that
-    # we import from breaking things *too* badly.  Also, save sys.path, 
-    # because we will modify if we're importing from a filename.  Note
-    # that we do *not* save sys.modules.
-    old_sys = sys.__dict__.copy()
-    old_sys_path = sys.path[:]
-    if type(__builtins__) == types.DictionaryType:
-        old_builtins = __builtins__.copy()
-    else:
-        old_builtins = __builtins__.__dict__.copy()
-
-    # Supress input and output.  (These get restored when we restore
-    # sys to old_sys).  
-    sys.stdin = sys.stdout = sys.stderr = _dev_null
-
-    # Remove any command-line arguments
-    sys.argv = ['(imported)']
-
-    try:
-        # If they gave us a file name, then convert it to a module
-        # name, and update the path appropriately.
-        if '/' in name or re.search('\.[pP][yY][cCoOwW]?$|\.[sS][oO]$', name):
-            if not os.path.exists(name):
-                raise ImportError('%r does not exist' % name)
-                return None
-            (basedir, name) = _find_module_from_filename(name)
-
-            # Make sure that the module is on the path.
-            sys.path = [basedir] + sys.path
-            
-        # Make sure that we have a valid name
-        if not re.match(r'^[a-zA-Z_]\w*(\.[a-zA-Z_]\w*)*$', name):
-            raise ImportError('Bad module name %r' % name)
-            
-        # Import the module.  Note that if "name" has a package
-        # component, then this just gives us the top-level object.
-        try:
-            topLevel = __import__(name)
-        except KeyboardInterrupt:
-            raise
-        except Exception, e:
-            raise ImportError('Could not import %r -- %s' % (name, e))
-        except SystemExit, e:
-            raise ImportError('Could not import %r -- %s' % (name, e))
-        except:
-            raise ImportError('Could not import %r')
-        
-        # If "name" has a package component, then we have to manually
-        # go down the package tree.
-        pieces = name.split(".")[1:]
-        m = topLevel
-        for p in pieces:
-            try: m = getattr(m, p)
-            except AttributeError:
-                estr = 'Could not import %r -- getattr failed' % name
-                raise ImportError(estr)
-        return m
-    finally:
-        # Restore the important values that we saved.
-        if type(__builtins__) == types.DictionaryType:
-            __builtins__.update(old_builtins)
-        else:
-            __builtins__.__dict__.update(old_builtins)
-        sys.__dict__.update(old_sys)
-        sys.path = old_sys_path
-
 if __name__ == '__main__':
     # A few quick tests.
     print import_module('os.path')
+    print import_module('distutils')
     print import_module('/usr/lib/python2.1/linecache.py')
     print import_module('/usr/lib/python2.1/distutils/cmd.py')
     print import_module('/usr/lib/python2.1/distutils/__init__.py')
-    print import_module('/usr/lib/python2.1/distutils/')
+    #print import_module('/usr/lib/python2.1/distutils/')
     print import_module('/usr/lib/python2.1/site-packages/'+
                         'Numeric/multiarray.so')
-    
+
     
