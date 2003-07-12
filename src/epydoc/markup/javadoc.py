761542a38a6d619a900e29813951f15b492038cf
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2003-07-12 00:57:26 +0000

    - Rewrote to use the new markup language processing package

--- src/epydoc/markup/javadoc.py
+++ src/epydoc/markup/javadoc.py
@@ -7,7 +7,7 @@
 #
 
 """
-Epydoc parser for L{Javadoc<http://java.sun.com/j2se/javadoc/>}
+Epydoc parser for U{Javadoc<http://java.sun.com/j2se/javadoc/>}
 docstrings.  Javadoc is an HTML-based markup language that was
 developed for documenting Java APIs.  The returned DOM tree will
 conform to the following Document Type Description::
@@ -40,107 +40,62 @@ __docformat__ = 'epytext en'
 # Imports
 import re
 from xml.dom.minidom import *
+from epydoc.markup import *
 
-def parse(docstring, field_warnings):
-    """
-    Parse the given docstring, which is formatted using JavaDoc
-    markup; and return an XML representation of its contents.  The
-    returned string is primarily composed of C{rawhtml} elements, with
-    C{link} elements embedded for Javadoc C{{@link}}s, and with
-    C{field} elements to encode Javadoc tagged fields.  See L{the
-    module documentation<epydoc.javadoc>} for more information.
-    """
-    doc = Document()
-    html_elt = doc.createElement('html')
-    doc.appendChild(html_elt)
-    docstring = docstring.strip()
-    if not docstring: return
+def parse_docstring(docstring, errors):
+    return ParsedJavadocDocstring(docstring)
 
-    # Split the docstring into a description and a list of fields.
-    (descr, fields) = _split_fields(docstring)
+class ParsedJavadocDocstring(ParsedDocstring):
+    def __init__(self, docstring):
+        self._docstring = docstring
 
-    # Add the description
-    descr_elt = doc.createElement('rawhtml')
-    html_elt.appendChild(descr_elt)
-    _javadoc_to_dom(descr, html_elt, doc, field_warnings)
-
-    # Add the fields
-    if fields:
-        fieldlist_elt = doc.createElement('fieldlist')
-        html_elt.appendChild(fieldlist_elt)
-        for (tag, arg, body) in fields:
-            field_elt = doc.createElement('field')
-            tag_elt = doc.createElement('tag')
-            fieldlist_elt.appendChild(field_elt)
-            field_elt.appendChild(tag_elt)
-            tag_elt.appendChild(doc.createTextNode(tag))
-            if arg is not None:
-                arg_elt = doc.createElement('arg')
-                field_elt.appendChild(arg_elt)
-                arg_elt.appendChild(doc.createTextNode(arg))
-            body_elt = doc.createElement('html')
-            field_elt.appendChild(body_elt)
-            _javadoc_to_dom(body, body_elt, doc, field_warnings)
-    
-    # Return the document
-    return doc
-
-# Which fields take arguments?
-_ARG_FIELDS = ('group variable var type cvariable cvar ivariable '+
-               'ivar return returns returntype rtype param parameter '+
-               'arg argument raise raises exception except '+
-               'deffield newfield').split()
-
-_FIELD_RE = re.compile(r'(^\s*\@\w+[\s$])', re.MULTILINE)
-def _split_fields(docstring):
-    descr = None
-    fields = []
-    
-    pieces = _FIELD_RE.split(docstring)
-    descr = pieces[0]
-    for i in range(1, len(pieces)):
-        if i%2 == 1:
-            tag = pieces[i].strip()[1:]
-        else:
-            if tag in _ARG_FIELDS:
-                (arg, val) = pieces[i].strip().split(None, 1)
+    # Which fields take arguments?
+    _ARG_FIELDS = ('group variable var type cvariable cvar ivariable '+
+                   'ivar return returns returntype rtype param '+
+                   'parameter arg argument raise raises exception '+
+                   'except deffield newfield').split()
+    _FIELD_RE = re.compile(r'(^\s*\@\w+[\s$])', re.MULTILINE)
+    def split_fields(self):
+        descr = None
+        fields = []
+        
+        pieces = self._FIELD_RE.split(self._docstring)
+        descr = ParsedJavadocDocstring(pieces[0])
+        for i in range(1, len(pieces)):
+            if i%2 == 1:
+                tag = pieces[i].strip()[1:]
             else:
-                (arg, val) = (None, pieces[i])
-            fields.append((tag, arg, val))
-    return (descr, fields)
+                if tag in self._ARG_FIELDS:
+                    (arg, body) = pieces[i].strip().split(None, 1)
+                else:
+                    (arg, body) = (None, pieces[i])
+                fields.append(Field(tag, arg, ParsedJavadocDocstring(body)))
+        return (descr, fields)
 
-_LINK_SPLIT_RE = re.compile(r'({@link(?:plain)? [^}]+})')
-_LINK_RE = re.compile(r'{@link(?:plain)? ' + r'([^\s\(]+)' +
-                      r'(?:\([^\)]*\))?' + r'(\s+.*)?' + r'}')
+    _LINK_SPLIT_RE = re.compile(r'({@link(?:plain)? [^}]+})')
+    _LINK_RE = re.compile(r'{@link(?:plain)? ' + r'([^\s\(]+)' +
+                          r'(?:\([^\)]*\))?' + r'(\s+.*)?' + r'}')
+    def to_html(self, docstring_linker, **options):
+        html = ''
+        pieces = _LINK_SPLIT_RE.split(self._docstring)
+        for i in range(len(pieces)):
+            if i%2 == 0:
+                html += pieces[i]
+            else:
+                m = _LINK_RE.match(pieces[i])
+                if m is None:
+                    field_warnings.append('Bad link: %r' % pieces[i])
+                    continue
+                (target, name) = m.groups()
+                if target[0] == '#': target = target[1:]
+                target = target.replace('#', '.')
+                target = re.sub(r'\(.*\)', '', target)
+                if name is None: name = target
+                else: name = name.strip()
+                html += docstring_linker.translate_identifier_xref(target,
+                                                                   name)
+        return html
 
-def _javadoc_to_dom(docstring, parent, doc, field_warnings):
-    pieces = _LINK_SPLIT_RE.split(docstring)
-
-    for i in range(len(pieces)):
-        if i%2 == 0:
-            rawhtml_elt = doc.createElement('rawhtml')
-            parent.appendChild(rawhtml_elt)
-            rawhtml_elt.appendChild(doc.createTextNode(pieces[i]))
-        else:
-            # Extract the name & the target.
-            m = _LINK_RE.match(pieces[i])
-            if m is None:
-                field_warnings.append('Bad link: %r' % pieces[i])
-                continue
-            (target, name) = m.groups()
-            if target[0] == '#': target = target[1:]
-            target = target.replace('#', '.')
-            target = re.sub(r'\(.*\)', '', target)
-            if name is None: name = target
-            else: name = name.strip()
-
-            # Construct the link element
-            link_elt = doc.createElement('link')
-            name_elt = doc.createElement('name')
-            target_elt = doc.createElement('target')
-            parent.appendChild(link_elt)
-            link_elt.appendChild(name_elt)
-            link_elt.appendChild(target_elt)
-            name_elt.appendChild(doc.createTextNode(name))
-            target_elt.appendChild(doc.createTextNode(target))
+    def to_plaintext(self, docstring_linker, **options):
+        return self._docstring
 
--- src/epydoc/markup/restructuredtext.py
+++ src/epydoc/markup/restructuredtext.py
@@ -8,29 +8,11 @@
 
 """
 Epydoc parser for ReStructuredText strings.  ReStructuredText is the
-markup language used by the Docutils project.
+standard markup language used by the Docutils project.
 
-This module defines a parser that converts ReStructuredText into an
-XML/DOM representation (using L{docutils}).  The returned DOM tree
-will conform to the following Document Type Description::
 
-   <!ELEMENT html ((rawhtml | link)*, fieldlist?)>
-   <!ELEMENT rawhtml (#PCDATA)>
-   
-   <!ELEMENT fieldlist (field+)>
-   <!ELEMENT field (tag, arg?, html)>
-   <!ELEMENT tag (#PCDATA)>
-   <!ELEMENT arg (#PCDATA)>
-   
-   <!ELEMENT link    (name, target)>
-   <!ELEMENT name    (#PCDATA)>
-   <!ELEMENT target  (#PCDATA)>
-
-This representation was chosen because it allows me to easily
-integrate ReStructuredText output into epydoc, while still supporting
-both crossreferences (marked with ReStructuredText interpreted text)
-and fields (marked with ReStructuredText fields).
-
+@todo: summary
+@todo: to_plaintext, etc?
 @warning: Epydoc only supports HTML output for ReStructuredText
 docstrings.
 """
@@ -39,12 +21,15 @@ __docformat__ = 'epytext en'
 # Imports
 import re
 from xml.dom.minidom import *
-from docutils.core import publish_string
-from docutils.writers.html4css1 import Writer as HTMLWriter
-from docutils.writers.html4css1 import HTMLTranslator
-from docutils.nodes import SkipChildren, SkipNode
 
-def parse(docstring):
+from docutils.core import publish_string
+from docutils.writers import Writer
+from docutils.writers.html4css1 import HTMLTranslator, Writer as HTMLWriter
+from docutils.nodes import NodeVisitor, TextElement, SkipChildren, SkipNode
+
+from epydoc.markup import *
+
+def parse_docstring(docstring, errors):
     """
     Parse the given docstring, which is formatted using
     ReStructuredText; and return an XML representation of its
@@ -52,241 +37,169 @@ def parse(docstring):
     elements, with C{link} elements embedded for ReStructuredText
     interpreted text (C{`...`}), and with C{field} elements to encode
     ReStructuredText fields.  See L{the module
-    documentation<epydoc.rst>} for more information.
+    documentation<epydoc.markup.restructuredtext>} for more information.
 
     @type docstring: C{string}
     @param docstring: The ReStructuredText docstring to parse.
     @rtype: L{xml.dom.minidom.Document}
     @return: An XML representation of C{docstring}'s contents.
     """
-    writer = _EpydocWriter()
+    writer = _DocumentPseudoWriter()
+    writer.settings_spec = HTMLWriter.settings_spec
     publish_string(docstring, writer=writer)
-    return writer.dom_document
+    return ParsedRstDocstring(writer.document)
+    
+class ParsedRstDocstring(ParsedDocstring):
+    def __init__(self, document):
+        self._document = document
 
-class _EpydocWriter(HTMLWriter):
+    def split_fields(self):
+        visitor = _SplitFieldsTranslator(self._document)
+        self._document.walk(visitor)
+        return self, visitor.fields
+
+    def summary(self):
+        visitor = _SummaryExtractor(self._document)
+        self._document.walk(visitor)
+        return visitor.summary
+
+    def to_html(self, docstring_linker, **options):
+        visitor = _EpydocHTMLTranslator(self._document, docstring_linker)
+        self._document.walkabout(visitor)
+        return ''.join(visitor.body)
+
+    def to_plaintext(self, docstring_linker, **options):
+        raise NotImplementedError, 'ParsedDocstring.to_plaintext()'
+
+    def __repr__(self): return '<ParsedRstDocstring: ...>'
+
+class _DocumentPseudoWriter(Writer):
     """
-    A simple writer class that uses an L{_EpydocTranslator} to produce
-    an XML representation of a ReStructuredText, which can be used by
-    epydoc to generate html output.
+    A pseudo-writer for the docutils framework, that can be used to
+    access the document itself.  The output of C{_DocumentPseudoWriter}
+    is just an empty string; but after it has been used, the most
+    recently processed document is available as the instance variable
+    C{document}
 
-    C{_EpydocWriter} is I{not} meant to be used in the \"standard\"
-    way with the docutils classes.  A typical writer would produce an
-    output as a string.  However, since we want our output to be an
-    XML tree, not a string, C{_EpydocWriter} generates its output as a
-    side-effect.  In paricular, C{_EpydocWriter} returns an empty
-    string; but the XML output can be accessed using the
-    L{dom_document} instance variable, after the writer has finished
-    processing the string.
-
-    I'm sure that there's a more standard way to generate XML output;
-    If someone who understands docutils better wants to rewrite this
-    code, I'd be happy to accept patches.
-
-    @type dom_document: C{xml.dom.minidom.Document}
-    @ivar dom_document: An XML document encoding the contents of the
-        most recently published string.
+    @type document: L{docutils.nodes.document}
+    @ivar document: The most recently processed document.
     """
     def __init__(self):
-        HTMLWriter.__init__(self)
-        self.translator_class = _EpydocTranslator
-        self.dom_document = None
-
+        self.document = None
+        Writer.__init__(self)
+        
     def translate(self):
-        visitor = self.translator_class(self.document)
-        self.document.walkabout(visitor)
-        self.dom_document = visitor.doc
         self.output = ''
-
-class _EpydocTranslator(HTMLTranslator):
+        
+class _SummaryExtractor(NodeVisitor):
     """
-    EpydocTranslator only makes a few changes to HTMLTranslator:
-
-      - Interpreted text nodes are converted to XML DOM C{link}
-        elements, instead of to html.
-      - Fields are converted to XML DOM C{field} elements, and stored
-        in the C{_fields} dictionary.
-      - All HTML is post-processed by L{_fixup} to ensure that it
-        doesn't conflict with epydoc.  In paritcular, local anchors
-        are renamed, css classes are renamed, and headings are
-        marked with css classes.
-      - Once the entire document has been processed (in
-        L{depart_document}), an XML DOM tree is generated to encode
-        the entire document.  The body is assembled from the HTML
-        strings and C{link} elements in the C{body} list; and the
-        fields are copied from the C{_fields} dictionary.  The final
-        result is stored in C{doc}
-    
-    @type doc: C{xml.dom.minidom.Document}
-    @ivar doc: An XML document encoding the contents of the
-        string that we're translating.
-    @ivar _fields: A dictionary mapping from (tag, arg) to
-        field contents.
-    @ivar _body_stack: A stack containing saved body contents.
-        When a field is entered, the current body contents are
-        pushed onto this stack; and when the field is exited,
-        they are popped back off.  This allows L{depart_field}
-        to get access to the html text for the field's contents.
+    A docutils node visitor that extracts the first sentence from
+    the first paragraph in a document.
     """
-
     def __init__(self, document):
-        HTMLTranslator.__init__(self, document)
-        self._body_stack = []
-        self._fields = []
-        self.doc = Document()
-
-    ##//////////////////////////////////////////////////
-    ## DOM Assmbly
-    ##//////////////////////////////////////////////////
-    
-    def visit_document(self, node): pass
-    def depart_document(self, node):
-        # Handle all fields
-        if self._fields:
-            fieldlist_elt = self.doc.createElement('fieldlist')
-            self.body.append(fieldlist_elt)
-            for (tag, arg, body) in self._fields:
-                field_elt = self.doc.createElement('field')
-                
-                tag_elt = self.doc.createElement('tag')
-                tag_elt.appendChild(self.doc.createTextNode(tag))
-                field_elt.appendChild(tag_elt)
-    
-                if arg is not None:
-                    arg_elt = self.doc.createElement('arg')
-                    arg_elt.appendChild(self.doc.createTextNode(arg))
-                    field_elt.appendChild(arg_elt)
-    
-                field_elt.appendChild(body)
-                fieldlist_elt.appendChild(field_elt)
-    
-        # Construc the final dom tree
-        self.doc.appendChild(self._body_to_dom(self.body))
-
-    def _body_to_dom(self, body, parent=None):
-        if parent is None:
-            parent = self.doc.createElement('html')
+        NodeVisitor.__init__(self, document)
+        self.summary = None
         
-        rawhtml_elt = None
-        for elt in body:
-            if type(elt) == type('') or type(elt) == type(u''):
-                # Raw HTML
-                if rawhtml_elt is None:
-                    rawhtml_elt = self.doc.createElement('rawhtml')
-                    parent.appendChild(rawhtml_elt)
-                    rawhtml_elt.appendChild(self.doc.createTextNode(''))
-                elt = self._fixup(elt)
-                rawhtml_elt.childNodes[-1].data += elt
-            elif isinstance(elt, xml.dom.minidom.Element):
-                # Link or Field.
-                rawhtml_elt = None
-                parent.appendChild(elt)
-            else:
-                raise ValueError, ('Bad body element %r' % elt)
+    def visit_document(self, node):
+        self.summary = None
+        
+    def visit_paragraph(self, node):
+        if self.summary is not None: return
+
+        summary_pieces = []
+        # Extract the first sentence.
+        for child in node.children:
+            if isinstance(child, Text):
+                m = re.match(r'(\s*[\w\W]*?\.)(\s|$)', child.data)
+                if m:
+                    summary_pieces.append(Text(m.group(1)))
+                    break
+            summary_pieces.append(child)
             
-        return parent
+        summary_doc = self.document.copy()
+        summary_doc.children = summary_pieces
+        self.summary = ParsedRstDocstring(summary_doc)
 
-    def _fixup(self, elt):
-        """
-        Post-process a piece of html that was generated by HTMLWriter,
-        to ensure that it doesn't conflict with epydoc.  In
-        particular, this adds a css class to headings, and prefixes
-        existing css classes, names, and local hrefs with C{'rst-'}.
-        """
-        # Prefix all classes, names, and local hrefs with "rst-"
-        elt = re.sub(r'(<[^>]+class=")([^"]+)', r'\1rst-\2', elt)
-        elt = re.sub(r'(<[^>]+name=")([^"]+)', r'\1rst-\2', elt)
-        elt = re.sub(r'(<[^>]+href="#)([^"]+)', r'\1rst-\2', elt)
+    def unknown_visit(self, node):
+        'Ignore all unknown nodes'
 
-        # Mark all headings with 'class="heading"'.
-        elt = re.sub(r'<h(\d+)>', r'<h\1 class="heading">', elt)
+class _SplitFieldsTranslator(NodeVisitor):
+    """
+    A docutils translator that removes all fields from a document, and
+    collects them into the instance variable C{fields}
+
+    @ivar fields: The fields of the most recently walked document.
+    @type fields: C{list} of L{Field<markup.Field>}
+    """
+    def __init__(self, document):
+        NodeVisitor.__init__(self, document)
+        self.fields = []
+
+    def visit_document(self, node):
+        self.fields = []
         
-        return elt
-                
-    ##//////////////////////////////////////////////////
-    ## Interpreted Text
-    ##//////////////////////////////////////////////////
-    
+    def visit_field_list(self, node):
+        # Remove the field list from the tree.
+        node.parent.remove(node)
+
+        # Add each field to self.fields
+        for field in node.children:
+            # Extract the field name & optional argument
+            tag = field.children[0].astext().split(None, 1)
+            name = tag[0]
+            if len(tag)>1: arg = name[1]
+            else: arg = None
+
+            # Extract the field body.
+            body = self.document.copy()
+            body.children = field.children[1].children
+            body = ParsedRstDocstring(body)
+
+            self.fields.append(Field(name, arg, body))
+        
+    def unknown_visit(self, node):
+        'Ignore all unknown nodes'
+
+class _EpydocHTMLTranslator(HTMLTranslator):
+    def __init__(self, document, docstring_linker):
+        HTMLTranslator.__init__(self, document)
+        self._linker = docstring_linker
+
+    # Handle interpreted text (crossreferences)
     def visit_title_reference(self, node):
-        # Get the target string.
-        target_str = self.encode(node.astext())
-        
-        # Create the link DOM elt (name=target=target_str)
-        link_elt = self.doc.createElement('link')
-        name_elt = self.doc.createElement('name')
-        name_elt.appendChild(self.doc.createTextNode(target_str))
-        target_elt = self.doc.createElement('target')
-        target_elt.appendChild(self.doc.createTextNode(target_str))
-        link_elt.appendChild(name_elt)
-        link_elt.appendChild(target_elt)
-
-        # Add the link to the body.
-        self.body.append(link_elt)
-
+        target = self.encode(node.astext())
+        xref = self._linker.translate_identifier_xref(target, target)
+        self.body.append(xref)
         raise SkipNode
 
-    ##//////////////////////////////////////////////////
-    ## Field Lists
-    ##//////////////////////////////////////////////////
-    
-    # Field lists are handled entirely by visit_field & depart_field
-    def visit_field_list(self, node): pass
-    def visit_field_body(self, node): pass
-    def depart_field_list(self, node): pass
-    def depart_field_body(self, node): pass
-    def visit_field_name(self, node): raise SkipNode
-
-    def visit_field(self, node):
-        self._body_stack.append(self.body)
-        self.body = []
+    def visit_document(self, node): pass
+    def depart_document(self, node): pass
         
-    def depart_field(self, node):
-        name=node.children[0].astext().split(None, 1)
-        tag = name[0]
-        if len(name)>1: arg = name[1]
-        else: arg = None
+    def starttag(self, node, tagname, suffix='\n', infix='', **attributes):
+        """
+        This modified version of starttag makes a few changes to HTML
+        tags, to prevent them from conflicting with epydoc.  In particular:
+          - existing class attributes are prefixed with C{'rst-'}
+          - existing names are prefixed with C{'rst-'}
+          - hrefs starting with C{'#'} are prefixed with C{'rst-'}
+          - all headings (C{<hM{n}>}) are given the css class C{'heading'}
+        """
+        # Prefix all CSS classes with "rst-"
+        if attributes.has_key('class'):
+            attributes['class'] = 'rst-%s' % attributes['class']
+
+        # Prefix all names with "rst-", to avoid conflicts
+        if attributes.has_key('id'):
+            attributes['id'] = 'rst-%s' % attributes['id']
+        if attributes.has_key('name'):
+            attributes['name'] = 'rst-%s' % attributes['name']
+        if attributes.has_key('href') and attributes['href'][:1]=='#':
+            attributes['href'] = '#rst-%s' % attributes['href'][1:]
+
+        # For headings, use class="heading"
+        if re.match(r'^h\d+$', tagname):
+            attributes['class'] = 'heading'
         
-        self._fields.append( (tag, arg, self._body_to_dom(self.body)) )
-        self.body = self._body_stack.pop()
-        
-def summary(epytext_doc):
-    """
-    Given a DOM document representing formatted documentation, return
-    a new DOM document containing the documentation's first sentence.
+        return HTMLTranslator.starttag(self, node, tagname, suffix,
+                                       infix, **attributes)
 
-    @param epytext_doc: A DOM document representing formatted
-        documentation, as produced by L{parse}.
-    @type epytext_doc: L{xml.dom.minidom.Document} or
-        L{xml.dom.minidom.Element}
-    @return: A DOM document containing the first sentence of the
-        documentation.
-    @rtype: L{xml.dom.minidom.Document}
-    """
-    doc = Document()
-    html = doc.createElement('html')
-    doc.appendChild(html)
-
-    if isinstance(epytext_doc, Document):
-        tree = epytext_doc.childNodes[0]
-    else:
-        tree = epytext_doc
-
-    # Extract the raw text (no html tags)
-    text = ''
-    for child in tree.childNodes:
-        if child.tagName == 'rawhtml':
-            text += re.sub('<[^>]*>', '', child.childNodes[0].data)
-        elif child.tagName == 'link':
-            text += child.childNodes[0].childNodes[0].data
-
-    # If we didn't find anything, return an empty document.
-    if text == '': return doc
-
-    # Extract the first sentence.
-    m = re.match(r'^(\s*[\w\W]*?[\.:])(\s|$)', text)
-    if m: text = m.group(1)
-
-    # Return an element containing the text.
-    rawhtml = doc.createElement('rawhtml')
-    html.appendChild(rawhtml)
-    rawhtml.appendChild(doc.createTextNode(text))
-    return doc
