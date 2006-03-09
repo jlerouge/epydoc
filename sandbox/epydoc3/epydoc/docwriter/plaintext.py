32d837440d8cd84172f9dfb09a38c1034c05b74a
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2006-03-09 01:28:34 +0000

    - Made some changes to bring the plaintext writer up-to-date

--- sandbox/epydoc3/epydoc/docwriter/plaintext.py
+++ sandbox/epydoc3/epydoc/docwriter/plaintext.py
@@ -7,55 +7,45 @@ import re
 
 class PlaintextWriter:
     def write(self, api_doc):
+        result = []
+        out = result.append
 
-        if isinstance(api_doc, ModuleDoc):
-            return self.write_module(api_doc)
-        if isinstance(api_doc, ClassDoc):
-            return self.write_class(api_doc, 0)
-        if isinstance(api_doc, RoutineDoc):
-            return self.write_function(api_doc, 0)
-        else:
-            assert 0, ('%s not handled yet' % api_doc.__class__)
+        try:
+            if isinstance(api_doc, ModuleDoc):
+                self.write_module(out, api_doc)
+            elif isinstance(api_doc, ClassDoc):
+                self.write_class(out, api_doc)
+            elif isinstance(api_doc, RoutineDoc):
+                self.write_function(out, api_doc)
+            else:
+                assert 0, ('%s not handled yet' % api_doc.__class__)
+        except Exception, e:
+            print '\n\n'
+            print ''.join(result)
+            raise
 
-    def write_module(self, mod_doc):
+        return ''.join(result)
+
+    def write_module(self, out, mod_doc):
         #for n,v in mod_doc.variables.items():
         #    print n, `v.value`, `v.value.value`
         
         # The cannonical name of the module.
-        s = self.section('Module Name', 0)
-        s += '    %s\n\n' % mod_doc.canonical_name
+        out(self.section('Module Name'))
+        out('    %s\n\n' % mod_doc.canonical_name)
 
         # The module's description.
         if mod_doc.descr not in (None, '', UNKNOWN):
-            s += self.section('Description', 0)
-            s += mod_doc.descr.to_plaintext(None, indent=4)
+            out(self.section('Description'))
+            out(mod_doc.descr.to_plaintext(None, indent=4))
 
-        s += 'metadata: %s\n\n' % mod_doc.metadata # [xx] testing
+        #out('metadata: %s\n\n' % mod_doc.metadata) # [xx] testing
 
-        # List of classes in this module.
-        classes = [v for v in mod_doc.sorted_variables
-                   if isinstance(v.value, ClassDoc) and
-                   v.is_imported in (False, UNKNOWN)]
-        s += self.write_classlist(classes, 'Classes', 0)
-
-        # List of functions in this module.
-        funcs = [v for v in mod_doc.sorted_variables
-                 if isinstance(v.value, FunctionDoc) and
-                 v.is_imported in (False, UNKNOWN)]
-        s += self.write_funclist(funcs, 'Functions', 0)
-        
-        # List of variables in this module
-        variables = [v for v in mod_doc.sorted_variables
-                     if not isinstance(v.value, (ClassDoc, FunctionDoc))]
-        localvars = [v for v in variables if
-                     v.is_imported in (False, UNKNOWN)]
-        s += self.write_varlist(localvars, 'Variables', 0)
-
-        impvars = [v for v in variables if
-                   v.is_imported is True]
-        s += self.write_varlist(impvars, 'Imported Variables', 0, False)
-        
-        return s
+        self.write_list(out, 'Classes', mod_doc, value_type='class')
+        self.write_list(out, 'Functions', mod_doc, value_type='function')
+        self.write_list(out, 'Variables', mod_doc, value_type='other')
+        # hmm.. do this as just a flat list??
+        #self.write_list(out, 'Imports', mod_doc, imported=True, verbose=False)
 
     def baselist(self, class_doc):
         if class_doc.bases is UNKNOWN:
@@ -68,167 +58,189 @@ class PlaintextWriter:
                 s += str(base.canonical_name[-1])
             else:
                 s += str(base.canonical_name)
-            if i < len(class_doc.bases)-1: s += ', '
+            if i < len(class_doc.bases)-1: out(', ')
         return s+')'
 
-    def write_class(self, class_doc, indent, name=None):
+    def write_class(self, out, class_doc, name=None, prefix='', verbose=True):
         baselist = self.baselist(class_doc)
         
         # If we're at the top level, then list the cannonical name of
         # the class; otherwise, our parent will have already printed
         # the name of the variable containing the class.
-        if indent == 0:
-            s = self.section('Class Name', 0)
-            s += '    %s%s\n\n' % (class_doc.canonical_name, baselist)
+        if prefix == '':
+            out(self.section('Class Name'))
+            out('    %s%s\n\n' % (class_doc.canonical_name, baselist))
         else:
-            s = indent*4*' ' + 'class %s' % self.bold(name) + baselist+'\n'
+            out(prefix + 'class %s' % self.bold(str(name)) + baselist+'\n')
 
-        if indent>0: indent += 1
+        if not verbose: return
+
+        # Indent the body
+        if prefix != '':
+            prefix += ' |  '
 
         # The class's description.
         if class_doc.descr not in (None, '', UNKNOWN):
-            if indent == 0:
-                s += self.section('Description', indent)
-                s += class_doc.descr.to_plaintext(None, indent=4+4*indent)
+            if prefix == '':
+                out(self.section('Description', prefix))
+                out(self._descr(class_doc.descr, '    '))
             else:
-                s += class_doc.descr.to_plaintext(None, indent=4*indent)
+                out(self._descr(class_doc.descr, prefix))
 
         # List of nested classes in this class.
-        classes = [v for v in class_doc.sorted_variables
-                   if isinstance(v.value, ClassDoc) and
-                   v.is_imported in (False, UNKNOWN)]
-        s += self.write_classlist(classes, 'Nested Classes', indent)
-
-        # List of instance methods in this class.
-        #print class_doc.sort_spec
-        #print class_doc.sorted_variables
-        funcs = [v for v in class_doc.sorted_variables
-                 if isinstance(v.value, InstanceMethodDoc) and
-                 v.is_imported in (False, UNKNOWN)]
-        s += self.write_funclist(funcs, 'Methods', indent)
-
-        # List of class methods in this class.
-        funcs = [v for v in class_doc.sorted_variables
-                 if isinstance(v.value, ClassMethodDoc) and
-                 v.is_imported in (False, UNKNOWN)]
-        s += self.write_funclist(funcs, 'Class Methods', indent)
-
-        # List of static methods in this class.
-        funcs = [v for v in class_doc.sorted_variables
-                 if isinstance(v.value, StaticMethodDoc) and
-                 v.is_imported in (False, UNKNOWN)]
-        s += self.write_funclist(funcs, 'Static Methods', indent)
-
-        # List of variables in this class
-        variables = [v for v in class_doc.sorted_variables
-                     if not isinstance(v.value,
-                                       (ClassDoc, InstanceMethodDoc,
-                                        ClassMethodDoc, StaticMethodDoc))
-                     and v.is_imported in (False, UNKNOWN)]
-        instvars = [v for v in variables if
-                    v.is_instvar in (UNKNOWN, True)]
-        s += self.write_varlist(instvars, 'Instance Variables', indent)
-        classvars = [v for v in variables if
-                     v.is_instvar is False]
-        s += self.write_varlist(classvars, 'Class Variables', indent)
-
-        if indent > 0:
-            s = self.drawline(s, indent*4-3)
-            
-        return s+'\n'
-
-    def write_variable(self, var_doc, indent):
-        s = '%s%s' % ('    '*indent, self.bold(var_doc.name))
+        self.write_list(out, 'Methods', class_doc,
+                        value_type='instancemethod', prefix=prefix,
+                        noindent=len(prefix)>4)
+        self.write_list(out, 'Class Methods', class_doc,
+                        value_type='classmethod', prefix=prefix)
+        self.write_list(out, 'Static Methods', class_doc,
+                        value_type='staticmethod', prefix=prefix)
+        self.write_list(out, 'Nested Classes', class_doc,
+                        value_type='class', prefix=prefix)
+        self.write_list(out, 'Instance Variables', class_doc,
+                        value_type='instancevariable', prefix=prefix)
+        self.write_list(out, 'Class Variables', class_doc,
+                        value_type='classvariable', prefix=prefix)
+        
+        self.write_list(out, 'Inherited Methods', class_doc,
+                        value_type='method', prefix=prefix,
+                        inherited=True, verbose=False)
+        self.write_list(out, 'Inherited Instance Variables', class_doc,
+                        value_type='instancevariable', prefix=prefix,
+                        inherited=True, verbose=False)
+        self.write_list(out, 'Inherited Class Variables', class_doc,
+                        value_type='classvariable', prefix=prefix,
+                        inherited=True, verbose=False)
+        self.write_list(out, 'Inherited Nested Classes', class_doc,
+                        value_type='class', prefix=prefix,
+                        inherited=True, verbose=False)
+                        
+    def write_variable(self, out, var_doc, name=None, prefix='', verbose=True):
+        if name is None: name = var_doc.name
+        out(prefix+self.bold(str(name)))
         if (var_doc.value not in (UNKNOWN, None) and
             var_doc.is_alias is True and var_doc.value.__class__ != ValueDoc):
-            s += ' = %s' % var_doc.value.canonical_name
+            out(' = %s' % var_doc.value.canonical_name)
         elif (var_doc.value not in (UNKNOWN, None) and
               var_doc.value.repr is not UNKNOWN):
-            if '\n' in var_doc.value.repr:
-                s += ' = %s...' % var_doc.value.repr.split('\n')[0]
-            else:
-                s += ' = %s' % var_doc.value.repr
-        if (var_doc.value not in (UNKNOWN, None) and
-            var_doc.value.imported_from is True):
-            s += ' (imported from %s)' % var_doc.value.imported_from
-        if (len(s)-len(var_doc.name)*2) > 75:
-            s = s[:72+len(var_doc.name)*2]+'...'
-        s += '\n'
+            val_repr = var_doc.value.repr.expandtabs()
+            if len(val_repr)+len(name) > 75:
+                val_repr = '%s...' % val_repr[:75-len(name)-3]
+            if '\n' in val_repr: val_repr = '%s...' % (val_repr.split()[0])
+            out(' = %s' % val_repr)
+        out('\n')
+        if not verbose: return
+        prefix += '    ' # indent the body.
         if var_doc.descr not in (None, '', UNKNOWN):
-            s += var_doc.descr.to_plaintext(None, indent=4+4*indent).rstrip()
-        return s.rstrip()+'\n'
+            out(self._descr(var_doc.descr, prefix))
 
-    def write_function(self, func_doc, indent, name=None):
-        if name is None: name = func_doc.canonical_name
-        s = self.signature(name, func_doc, indent)
-        if func_doc.descr not in (None, '', UNKNOWN):
-            s += func_doc.descr.to_plaintext(None, indent=4+4*indent)
+    def write_property(self, out, prop_doc, name=None, prefix='',
+                       verbose=True):
+        if name is None: name = prop_doc.canonical_name
+        out(prefix+self.bold(str(name)))
+        if not verbose: return
+        prefix += '    ' # indent the body.
             
+        if prop_doc.descr not in (None, '', UNKNOWN):
+            out(self._descr(prop_doc.descr, prefix))
+
+
+    def write_function(self, out, func_doc, name=None, prefix='',
+                       verbose=True):
+        if name is None: name = func_doc.canonical_name
+        self.write_signature(out, func_doc, name, prefix)
+        if not verbose: return
+        
+        prefix += '    ' # indent the body.
+            
+        if func_doc.descr not in (None, '', UNKNOWN):
+            out(self._descr(func_doc.descr, prefix))
+
         if func_doc.return_descr not in (None, '', UNKNOWN):
-            s += self.section('Returns:', indent+1)
-            s += func_doc.return_descr.to_plaintext(None, indent=8+4*indent)
+            out(self.section('Returns:', prefix))
+            out(self._descr(func_doc.return_descr, prefix+'    '))
 
         if func_doc.return_type not in (None, '', UNKNOWN):
-            s += self.section('Return Type:', indent+1)
-            s += func_doc.return_type.to_plaintext(None, indent=8+4*indent)
-            
-        return s.rstrip()+'\n\n'
+            out(self.section('Return Type:', prefix))
+            out(self._descr(func_doc.return_type, prefix+'    '))
 
-    def signature(self, name, func_doc, indent):
-        args = [self.write_arg(argname, default) for (argname, default) 
+    def write_signature(self, out, func_doc, name, prefix):
+        args = [self.fmt_arg(argname, default) for (argname, default) 
                 in zip(func_doc.posargs, func_doc.posarg_defaults)]
         if func_doc.vararg: args.append('*'+func_doc.vararg)
         if func_doc.kwarg: args.append('**'+func_doc.kwarg)
 
-        s = '%s%s(' % ('    '*indent, self.bold(str(name)))
-        left = indent*4+len(name)+1
-        x = left
+        out(prefix+self.bold(str(name))+'(')
+        x = left = len(prefix) + len(name) + 1
         for i, arg in enumerate(args):
             if x > left and x+len(arg) > 75:
-                s += '\n'+' '*left
+                out('\n'+prefix + ' '*len(name) + ' ')
                 x = left
-            s += arg
+            out(arg)
             x += len(arg)
             if i < len(args)-1:
-                s += ', '
+                out(', ')
                 x += 2
-        return s+')\n'
+        out(')\n')
 
-    def write_arg(self, name, default):
+    # [xx] tuple args!
+    def fmt_arg(self, name, default):
         if default is None:
-            return name
+            return '%s' % name
         elif default.repr is not UNKNOWN:
             return '%s=%s' % (name, default.repr)
         else:
             return '%s=??' % name
 
-    def write_varlist(self, vardocs, title, indent, doublespace=True):
-        s = ''
-        for var_doc in vardocs:
-            s += self.write_variable(var_doc, indent+1)
-            if doublespace: s += '\n'
-        if not s: return s
-        else: return self.section(title, indent)+s
+    def write_list(self, out, heading, doc, value_type=None, imported=False,
+                   inherited=False, prefix='', noindent=False,
+                   verbose=True):
+        # Get a list of the VarDocs we should describe.
+        if isinstance(doc, ClassDoc):
+            var_docs = doc.select_variables(value_type=value_type,
+                                            imported=imported,
+                                            inherited=inherited)
+        else:
+            var_docs = doc.select_variables(value_type=value_type,
+                                            imported=imported)
+        if not var_docs: return
 
-    def write_classlist(self, vardocs, title, indent):
-        s = ''
-        for var_doc in vardocs:
-            s += self.write_class(var_doc.value, indent+1,
-                                   var_doc.name)
-        if not s: return s
-        else: return self.section(title, indent)+s
+        out(prefix+'\n')
+        if not noindent:
+            out(self.section(heading, prefix))
+            prefix += '    '
 
-    def write_funclist(self, vardocs, title, indent):
-        s = ''
-        for var_doc in vardocs:
-            s += self.write_function(var_doc.value, indent+1,
-                                      var_doc.name)
-        if not s: return s
-        else: return self.section(title, indent)+s
+        for i, var_doc in enumerate(var_docs):
+            val_doc, name = var_doc.value, var_doc.name
 
-    def drawline(self, s, x):
-        s = re.sub(r'(?m)^(.{%s}) ' % x, r'\1|', s)
-        return re.sub(r'(?m)^( {,%s})$(?=\n)' % x, x*' '+'|', s)
+            if verbose:
+                out(prefix+'\n')
+
+            # hmm:
+            if not verbose:
+                if isinstance(doc, ClassDoc):
+                    name = var_doc.canonical_name
+                elif val_doc not in (None, UNKNOWN):
+                    name = val_doc.canonical_name
+                    
+            if isinstance(val_doc, RoutineDoc):
+                self.write_function(out, val_doc, name, prefix, verbose)
+            elif isinstance(val_doc, PropertyDoc):
+                self.write_property(out, val_doc, name, prefix, verbose)
+            elif isinstance(val_doc, ClassDoc):
+                self.write_class(out, val_doc, name, prefix, verbose)
+            else:
+                self.write_variable(out, var_doc, name, prefix, verbose)
+
+    def _descr(self, descr, prefix):
+        s = descr.to_plaintext(None, indent=len(prefix)).rstrip()
+        s = '\n'.join([(prefix+l[len(prefix):]) for l in s.split('\n')])
+        return s+'\n'#+prefix+'\n'
+                               
+
+#    def drawline(self, s, x):
+#        s = re.sub(r'(?m)^(.{%s}) ' % x, r'\1|', s)
+#        return re.sub(r'(?m)^( {,%s})$(?=\n)' % x, x*' '+'|', s)
 
         
     #////////////////////////////////////////////////////////////
@@ -242,10 +254,10 @@ class PlaintextWriter:
     def title(self, text, indent):
         return ' '*indent + self.bold(text.capitalize()) + '\n\n'
 
-    def section(self, text, indent):
-        if indent == 0:
-            return '    '*indent + self.bold(text.upper()) + '\n'
+    def section(self, text, indent=''):
+        if indent == '':
+            return indent + self.bold(text.upper()) + '\n'
         else:
-            return '    '*indent + self.bold(text.capitalize()) + '\n'
+            return indent + self.bold(text.capitalize()) + '\n'
 
 
