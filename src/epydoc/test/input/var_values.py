3f619cfe15863d006053b1e662128959fc219515
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2006-03-10 00:39:49 +0000

    moving 3.0 out of the sandbox

--- src/epydoc/test/input/var_values.py
+++
@@ -1,7 +0,0 @@
-"""Test for unicode and other troubles in variable values and regexp patterns."""
-import re
-
-# from Django -- http://www.djangoproject.com
-DOTS = ['&middot;', '*', '\xe2\x80\xa2', '&#149;', '&bull;', '&#8226;']
-ustring_re = re.compile(u"([\u0080-\uffff])")
-hard_coded_bullets_re = re.compile(r'((?:<p>(?:%s).*?[a-zA-Z].*?</p>\s*)+)' % '|'.join([re.escape(d) for d in DOTS]), re.DOTALL)
