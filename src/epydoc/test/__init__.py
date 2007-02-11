028c033de680fb3cf5d440e9044a557534b2c14e
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2007-02-11 05:25:21 +0000

    - Added support for adding ':RequireModule:' fields to doctest test   files, which assert that the tests shouldn't be run unless that   module is available.  Used by restructuredtext.doctest to prevent   the tests from being run if the docutils module is not installed.

--- src/epydoc/test/__init__.py
+++ src/epydoc/test/__init__.py
@@ -11,7 +11,7 @@ Regression testing.
 """
 __docformat__ = 'epytext en'
 
-import unittest, doctest, epydoc, os, os.path
+import unittest, doctest, epydoc, os, os.path, re
 
 def main():
     # Turn on debugging.
@@ -26,11 +26,35 @@ def main():
     testdir = os.path.join(os.path.split(__file__)[0])
     if testdir == '': testdir = '.'
     for filename in os.listdir(testdir):
-        if filename.endswith('.doctest'):
+        if (filename.endswith('.doctest') and
+            check_requirements(os.path.join(testdir, filename))):
             tests.append(doctest.DocFileSuite(filename, optionflags=options))
             
     # Run all test cases.
     unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(tests))
+
+def check_requirements(filename):
+    """
+    Search for strings of the form:
+    
+        [Require: <module>]
+
+    If any are found, then try importing the module named <module>.
+    If the import fails, then return False.  If all required modules
+    are found, return True.  (This includes the case where no
+    requirements are listed.)
+    """
+    s = open(filename).read()
+    for m in re.finditer('(?mi)^[ ]*\:RequireModule:[ ]+(\w+)[ ]*$', s):
+        module = m.group(1)
+        try:
+            __import__(module)
+        except ImportError:
+            print ('Skipping %r (required module %r not found)' %
+                   (os.path.split(filename)[-1], module))
+            return False
+    return True
+            
 
 if __name__=='__main__':
     main()
