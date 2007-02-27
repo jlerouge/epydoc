e25b792747f16c9d4c0920c8e5104498729e5616
edloper <edloper@dcb71dd9-490e-0410-a59b-d6317640170d>
2007-02-27 05:28:35 +0000

    - Bumped version alpha3 -> beta1

--- src/epydoc/__init__.py
+++ src/epydoc/__init__.py
@@ -167,7 +167,7 @@ information about the submodules and subpackages.
 
 :author: `Edward Loper <edloper@gradient.cis.upenn.edu>`__
 :requires: Python 2.3+
-:version: 3.0 alpha 2
+:version: 3.0 beta 1
 :see: `The epydoc webpage <http://epydoc.sourceforge.net>`__
 :see: `The epytext markup language
     manual <http://epydoc.sourceforge.net/epytext.html>`__
@@ -200,7 +200,7 @@ information about the submodules and subpackages.
 """
 __docformat__ = 'restructuredtext en'
 
-__version__ = '3.0alpha3'
+__version__ = '3.0beta1'
 """The version of epydoc"""
 
 __author__ = 'Edward Loper <edloper@gradient.cis.upenn.edu>'
